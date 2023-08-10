419af1541ec96e7d017e7f8c1df2ebdc9006b961
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-08-10 23:57:03 +0530

    made changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
         {/* Header */}
        <LeftSidebar/>
        <main className="ml-[275px] p-6 flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-        <h1 className="text-xl font-bold">Home</h1>
+        <h1 className="text-xl font-bold my-4">Home</h1>
         <div className="border-t-[0.5px] border-b-[0.5px] flex items-stretch py-4 space-x-2 border-gray-600 h-32 relative">
           <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
           <div className="flex flex-col w-full h-full ">
